1a98a2a9ca755518b06d53476861e4336b730501
Airasu <aira.suranol02@gmail.com>
2024-09-16 08:08:14 +0800

    added more pages

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,13 +1,23 @@
 from django.urls import path
-from .views import Home, Register, Signin, Dashboard, Index, Register2, Members_card
+from .views import Home, Register, Signin, Dashboard, Register2, Membership_card, Announcement, Linked, MembersReactivation, MembersModification, MembershipCard, Attendance, Events, Transactions, Products, ClearanceStatus, Archive, Reports, Settings
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
     path("register", Register.as_view(), name="Register"),
     path("signin", Signin.as_view(), name="Signin"),
     path("dashboard", Dashboard.as_view(), name="Dashboard"),
-    path("index", Index.as_view(), name="Index"),
     path("register2", Register2.as_view(), name="Register2"),
-    path("members_card", Members_card.as_view(), name="Members_card"),
-
+    path("linked", Linked.as_view(), name="Linked"),
+    path("members_reactivation", MembersReactivation.as_view(), name="MembersReactivation"),
+    path("members_modification", MembersModification.as_view(), name="MembersModification"),
+    path("membershipcard", MembershipCard.as_view(), name="MembershipCard"),
+    path("attendance", Attendance.as_view(), name="Attendance"),
+    path("events", Events.as_view(), name="Events"),
+    path("transactions", Transactions.as_view(), name="Transactions"),
+    path("products", Products.as_view(), name="Products"),
+    path("clearance_status", ClearanceStatus.as_view(), name="ClearanceStatus"),
+    path("archive", Archive.as_view(), name="Archive"),
+    path("reports", Reports.as_view(), name="Reports"),
+    path("settings", Settings.as_view(), name="Settings")
 ]
+
--- user_app/views.py
+++ user_app/views.py
@@ -16,11 +16,50 @@ class Signin(TemplateView):
 class Dashboard(TemplateView):
     template_name = "pages/dashboard.html"
 
-class Index(TemplateView):
-    template_name = "pages/index.html"
 
-    
-class Members_card(TemplateView):
-    template_name = "pages/members_card.html"
+def Index(request):
+    return render(request, 'pages/index.html')
 
 
+class Membership_card(TemplateView):
+    template_name = "pages/membership_card.html"
+
+
+class Announcement(TemplateView):
+    template_name = "pages/announcement.html"
+
+class Linked(TemplateView):
+    template_name = "pages/linked.html"
+
+class MembersReactivation(TemplateView):
+    template_name = "pages/members_reactivation.html"
+
+class MembersModification(TemplateView):
+    template_name = "pages/members_modification.html"
+
+class MembershipCard(TemplateView):
+    template_name ="pages/membershipcard.html"
+
+class Attendance(TemplateView):
+    template_name = "pages/attendance.html"
+
+class Events(TemplateView):
+    template_name ="pages/events.html"
+
+class Transactions(TemplateView):
+    template_name = "pages/transaction.html"
+
+class Products(TemplateView):
+    template_name ="pages/products.html"
+
+class ClearanceStatus(TemplateView):
+    template_name = "pages/clearance_status.html"
+
+class Archive(TemplateView):
+    template_name ="pages/archive.html"
+
+class Reports(TemplateView):
+    template_name ="pages/reports.html"
+
+class Settings(TemplateView):
+    template_name ="pages/settings.html"
