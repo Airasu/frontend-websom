c6b80cbc66d5cece59e047a7065cf585957e6209
me10die <gardosemelodie@gmail.com>
2024-09-25 02:29:33 +0800

    functional clearance records -clearance-

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,23 +1,24 @@
 from django.urls import path
-from .views import Home, Register, Signin, Announcement, Linked, MembersReactivation, MembersModification, MembershipCard, Attendance, Events, Transactions, Products, ClearancePage1, Archive, Reports, Settings, Dashboard, ClearancePage2, ClearancePage3
+from .views import Home, Register, Sign_in, Announcement, Linked, o_reactivate_member, MembersModification, MembershipCard, o_attendance, Events, Transactions, Products, o_clearance_clearance, Archive, Reports, Settings, Dashboard, o_clearance_rec_processing, o_clearance_rec_requirements, o_attendance_add
 urlpatterns = [
     path("", Home.as_view(), name="home"),
-    path("register", Register.as_view(), name="Register"),
-    path("signin", Signin.as_view(), name="Signin"),
+    path("register", Register.as_view(), name="register"),
+    path("sign_in", Sign_in.as_view(), name="sign_in"),
     path("linked", Linked.as_view(), name="Linked"),
-    path("members_reactivation", MembersReactivation.as_view(), name="MembersReactivation"),
+    path("o_reactivate_member", o_reactivate_member.as_view(), name="o_reactivate_member"),
     path("members_modification", MembersModification.as_view(), name="MembersModification"),
     path("membershipcard", MembershipCard.as_view(), name="MembershipCard"),
-    path("attendance", Attendance.as_view(), name="Attendance"),
+    path("o_attendance", o_attendance.as_view(), name="o_attendance"),
     path("events", Events.as_view(), name="Events"),
     path("transactions", Transactions.as_view(), name="Transactions"),
     path("products", Products.as_view(), name="Products"),
-    path("clearance_page1", ClearancePage1.as_view(), name="ClearancePage1"),
+    path("o_clearance_clearance", o_clearance_clearance.as_view(), name="o_clearance_clearance"),
     path("archive", Archive.as_view(), name="Archive"),
     path("reports", Reports.as_view(), name="Reports"),
     path("settings", Settings.as_view(), name="Settings"),
     path("create_announcement", Announcement.as_view(), name="CreateAnnouncement"),
-    path("dashboard", Dashboard.as_view(), name="Dashboard"),
-    path("clearance_page2", ClearancePage2.as_view(), name="ClearancePage2"),
-    path("clearance_page3", ClearancePage3.as_view(), name="ClearancePage3")
+    path("o_dashboard", Dashboard.as_view(), name="o_dashboard"),
+    path("o_clearance_rec_processing", o_clearance_rec_processing.as_view(), name="o_clearance_rec_processing"),
+    path("o_clearance_rec_requirements", o_clearance_rec_requirements.as_view(), name="o_clearance_rec_requirements"),
+    path("o_attendance_add", o_attendance_add.as_view(), name="o_attendance_add")
 ]
--- user_app/views.py
+++ user_app/views.py
@@ -7,14 +7,14 @@ class Home(TemplateView):
 class Register(TemplateView):
     template_name = "pages/register.html"
 
-class Signin(TemplateView):
-    template_name = "pages/signin.html"
+class Sign_in(TemplateView):
+    template_name = "pages/sign_in.html"
 
 class Linked(TemplateView):
     template_name = "pages/linked.html"
 
-class MembersReactivation(TemplateView):
-    template_name = "pages/members_reactivation.html"
+class o_reactivate_member(TemplateView):
+    template_name = "pages/o_reactivate_member.html"
 
 class MembersModification(TemplateView):
     template_name = "pages/members_modification.html"
@@ -22,8 +22,8 @@ class MembersModification(TemplateView):
 class MembershipCard(TemplateView):
     template_name ="pages/membershipcard.html"
 
-class Attendance(TemplateView):
-    template_name = "pages/attendance.html"
+class o_attendance(TemplateView):
+    template_name = "pages/o_attendance.html"
 
 class Events(TemplateView):
     template_name ="pages/events.html"
@@ -34,8 +34,8 @@ class Transactions(TemplateView):
 class Products(TemplateView):
     template_name ="pages/products.html"
 
-class ClearancePage1(TemplateView):
-    template_name = "pages/clearance_page1.html"
+class o_clearance_clearance(TemplateView):
+    template_name = "pages/o_clearance_rec_clearance.html"
 
 class Archive(TemplateView):
     template_name ="pages/archive.html"
@@ -50,10 +50,13 @@ class Announcement(TemplateView):
     template_name ="pages/create_announcements.html"
 
 class Dashboard(TemplateView):
-    template_name ="pages/dashboard.html"
+    template_name ="pages/o_dashboard.html"
 
-class ClearancePage2(TemplateView):
-    template_name ="pages/clearance_page2.html"
+class o_clearance_rec_processing(TemplateView):
+    template_name ="pages/o_clearance_rec_processing.html"
 
-class ClearancePage3(TemplateView):
-    template_name ="pages/clearance_page3.html"
+class o_clearance_rec_requirements(TemplateView):
+    template_name ="pages/o_clearance_rec_requirements.html"
+
+class o_attendance_add(TemplateView):
+    template_name ="pages/o_attendance_add.html"
